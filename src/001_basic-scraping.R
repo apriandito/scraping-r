ef6a3d8f25eaef49b8183bff3d132c433232792c
apriandito <m.apriandito@gmail.com>
2021-10-05 17:51:52 +0700

    first commit

---
+++ src/001_basic-scraping.R
@@ -0,0 +1,30 @@
+# Load Packages
+library(rvest)
+library(tidyverse)
+library(lubridate)
+
+# Set URL
+url <- read_html("https://www.imdb.com/search/title/?title_type=feature&primary_language=id&sort=year,asc&start=0")
+
+# Mendapatkan Judul Film
+judul_film <- url %>%
+  html_elements(".lister-item-header a") %>%
+  html_text()
+
+# Mendapatkan Tahun Rilis
+tahun_rilis <- url %>%
+  html_elements(".text-muted.unbold") %>%
+  html_text()
+
+# Mendapatkan daftar sutradara
+sutradara <- url %>%
+  html_elements(".text-muted+ p a:nth-child(1)") %>%
+  html_text() 
+
+# Buat sebagai sebuah data frame (Eror karena ada informasi sutradara yang kosong)
+film_indonesia <- tibble(judul_film = judul_film, 
+                         tahun_rilis = tahun_rilis, 
+                         sutradara = sutradara)
+
+
+  
